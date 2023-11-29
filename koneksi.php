575795f1208f4460b733d256b8ace7bca0f5dda2
Imuyyy <137760196+Imuyyy@users.noreply.github.com>
2023-11-29 16:51:36 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$database_hostname = "localhost";
+$database_username = "root";
+$database_password = "";
+$database_name = "sttb";
+$database_port = 3306;
+
+try {
+    $databse_connection = new PDO("mysql:host=$database_hostname;port=$database_port;dbname=$database_name",$database_username,$database_password);
+    // echo "Koneksi Berhasil";
+} catch (PDOException $x) {
+    echo $x->getMessage(); 
+}
